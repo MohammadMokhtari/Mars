1e0f6557ba28e919e3c0a3754e2234431c8ecd64
Mohammad Mokhtari <dev.mohammadmokhtari@gmail.com>
2021-02-14 11:09:29 +0330

    Add form login in index.html and create function login js/app.js

--- js/app.js
+++ js/app.js
@@ -12,4 +12,13 @@ $(document).ready(function () {
   $(selector).click(function (e) {
     e.preventDefault();
   });
+
+  // * For login Page in index.html
+  $.ajax({
+    type: "method",
+    url: "url",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {},
+  });
 });
